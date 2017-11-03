02afe471d09e98b21b3406ced7e0d0986db95f3c
Oscar <oscar.romin@gmail.com>
2017-11-03 16:25:29 +0100

    init checkin
    init checkin

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Room : MonoBehaviour
+{
+    public string Name;
+    public string Description;
+    public Image Image;
+}
+
+public static class ExtensionMethods
+{
+    public static GameObject FindObject(this GameObject parent, string name)
+    {
+        Transform[] trs = parent.GetComponentsInChildren<Transform>(true);
+        foreach (Transform t in trs)
+        {
+            if (t.name == name)
+            {
+                return t.gameObject;
+            }
+        }
+        return null;
+    }
+}
